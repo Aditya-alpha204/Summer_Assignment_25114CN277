5fbfd8ad77192e6c4ab9f7f4fe5f1007f438ce68
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-03 23:08:59 +0530

    Print prime number

---
+++ Day-3/question2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    int r;
+    printf("Enter range:");
+    scanf("%d",&r);
+
+
+    for(int j=2;j<=r;j++){
+        int is_prime=1;
+        for(int i=2;i<=j/2;i++){
+            if(j%i==0){
+                is_prime==0;
+                break;
+            }
+        }
+        if(is_prime==1){
+            printf("%d ",j);
+        }
+    }
+}
