739268398dcb0e0cf647d7d37a0cd64caff35a1f
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-05 21:53:41 +0530

    Largest Prime Factor

---
+++ Day-5/question4.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <math.h>
+
+long long largestPrimeFactor(long long n) {
+    long long maxPrime = -1;
+
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n /= 2;
+    }
+
+    for (long long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n /= i;
+        }
+    }
+
+    if (n > 2) {
+        maxPrime = n;
+    }
+
+    return maxPrime;
+}
+
+int main() {
+    long long n;
+    
+    printf("Enter a positive integer: ");
+    if (scanf("%lld", &n) != 1 || n <= 1) {
+        printf("Please enter an integer greater than 1.\n");
+        return 1;
+    }
+
+    long long result = largestPrimeFactor(n);
+    printf("The largest prime factor of %lld is %lld\n", n, result);
+
+    return 0;
+}
