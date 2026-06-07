09bf47cfb2847e5ce67b4c808bf045703a36615f
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-07 19:48:53 +0530

    Recursive Factorial

--- Day-7/question1.c
+++ Day-7/question1.c
@@ -7,13 +7,12 @@ fact(int n){
     else{
         return n*fact(n-1);
     }
-
-    int main(){
+}
+int main(){
         int n;
-        printf("Enter a number");
+        printf("Enter a number: ");
         scanf("%d",&n);
         int result=fact(n);
         printf("The factorial of %d is %d ",n,result);
         return 0;
     }
-}
