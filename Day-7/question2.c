2935d50ca060669bff85cd9a9d124d13d9389108
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-08 00:16:42 +0530

    Sum of digits

--- Day-7/question2.c
+++ Day-7/question2.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-int fibonacci(int n) {
+int sum(int n) {
     if (n == 0) {
         return 0;
     } 
-    else if (n == 1) {
-        return 1;
-    }
     else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        int r=n%10;
+        n=n/10;
+        return r+sum(n);
     }
 }
 int main() {
-    int terms;
+    int n;
     
-    printf("Enter the number of terms for the series: ");
-    scanf("%d", &terms);
-    
-    printf("Fibonacci Series: ");
-    for (int i = 0; i < terms; i++) {
-        printf("%d ", fibonacci(i));
-    }
-    
-    printf("\n");
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    printf("The sum of digits is %d",sum(n));
     return 0;
 }
