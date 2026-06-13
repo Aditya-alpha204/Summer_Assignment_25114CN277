20d173fc768c52c6996b183df04490e27b0d39e2
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-13 14:18:05 +0530

    Largest and smallest

---
+++ Day-13/question3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+void main(){
+    printf("Enter size of array: ");
+    int size,s=0;
+    float avg;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        printf("Enter %d element: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    int large=arr[0];
+    int small=arr[0];
+    for(int i=1;i<size;i++){
+        if(arr[i]>large){
+            large=arr[i];
+        }
+        if(arr[i]<small){
+            small=arr[i];
+        }
+          
+    }
+    printf("%d is largest\n",large);
+    printf("%d is smallest",small);
+
+}
