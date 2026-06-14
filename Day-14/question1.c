7ab1579e34c32fabd2f7df5edbb94a12a3130355
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-14 12:54:16 +0530

    Linear search

---
+++ Day-14/question1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    printf("Enter size of array: ");
+    int size;
+    scanf("%d",&size);
+    char arr[size];
+    for(int i=0;i<size;i++){
+        printf("Enter %d element: ",i+1);
+        scanf(" %c",&arr[i]);
+    }
+    char k;
+    int e=-1;
+    printf("Enter character to search: ");
+    scanf(" %c",&k);
+    for(int i=0;i<size;i++){
+        if(arr[i]==k){
+            e=i;
+            break;
+        }
+    }
+    if(e>=0){
+        printf("the %c is at %d position",k,e);
+    }
+    else{
+        printf("Not Found");
+    }
+    return 0;
+
+}
