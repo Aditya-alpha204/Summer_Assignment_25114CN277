e2f5b5070148840fcd719edd6b164ea65622b1b0
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-20 16:46:31 +0530

    Subtract matrx

---
+++ Day-19/question2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int rows,columns;
+    printf("Enter number of  rows of matrix\n ");
+    scanf("%d",&rows);
+    printf("Enter number of columns of matrix\n ");
+      scanf("%d",&columns);
+    printf("Enter elements of Matrix A\n");
+    int arr[rows][columns];
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            printf("Enter element of (%d,%d)\n",i,j);
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     printf("Enter elements of Matrix B\n");
+    int brr[rows][columns];
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            printf("Enter element of (%d,%d)\n",i,j);
+            scanf("%d",&brr[i][j]);
+        }
+    }
+    int crr[rows][columns];
+    printf("The subtraction of matrices  is\n");
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            crr[i][j]=arr[i][j]-brr[i][j];
+            printf("%d ",crr[i][j]);
+
+        }
+        printf("\n");
+    }
+
+}
