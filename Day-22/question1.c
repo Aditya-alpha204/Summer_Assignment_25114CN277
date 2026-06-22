82115bcea65dc4b3be2d49156085565cd7056f81
Aditya Kumar Yadav <219243300+Aditya-alpha204@users.noreply.github.com>
2026-06-22 14:49:25 +0530

    Palindrome word

---
+++ Day-22/question1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    char str[50];
+    printf("Enter a string :\n");
+    scanf("%s",str);
+    int l=0;
+    while(str[l]!='\0'){
+        l++;
+    }
+    int left=0,right=l-1;
+    int palin=0;
+    while(left<right){
+       if(str[left]==str[right]){
+        left++;
+        right--;
+       }
+       else{
+        palin++;
+       }
+    }
+    if(palin==0){
+        printf("It is palindrome word");
+    }
+    else{
+        printf("It is not palindrome word");
+    }
+
+}
